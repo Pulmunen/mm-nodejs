0800d2652e63ac758ca50246d4b68ca0b00838e6
Pulmunen <github.com.mustiness480@passmail.net>
2024-10-11 13:18:31 +0300

    uses pug templating

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const http = require('http');
 
-const routes = require('./routes')
+const path = require('path');
+const express = require('express');
 
-const server = http.createServer(routes.handler)
+const app = express();
 
-server.listen(3000);
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle:'Page not found'})
+});
+
+app.listen(3002);
