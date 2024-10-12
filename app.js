8d86d67e5d5eb317ce159686aedb177124a53797
Pulmunen <github.com.mustiness480@passmail.net>
2024-10-12 17:00:29 +0300

    end of working with file system

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-
 const path = require('path');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
+const db = require('./util/database')
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(express.urlencoded({extended:true}))
+db.execute('SELECT * FROM products')
+.then((result)=>{
+	console.log(result[0])
+})
+.catch((err)=>{
+	console.log(err)
+})
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle:'Page not found'})
-});
+app.use(errorController.get404);
 
-app.listen(3002);
+app.listen(3000);
